e136003e1966625a13fbcec175b41914c96facf8
Kai Chang <kaijchang@gmail.com>
2020-05-29 23:56:35 -0700

    change transformed format

--- data/mit-election-lab/main.py
+++ data/mit-election-lab/main.py
@@ -1,49 +1,55 @@
-import os
-
 import csv
 
-mit_election_lab_data_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'mit-election-lab')
+import json
 
 
 def read_row(headers, row):
     return dict(zip(headers, row))
 
 
-with open(os.path.join(mit_election_lab_data_path, '1976-2016-president.csv')) as presidential_returns_file:
+with open('raw/1976-2016-president.csv') as presidential_returns_file:
     presidential_returns_reader = csv.reader(presidential_returns_file)
     presidential_return_headers = next(presidential_returns_reader)
     presidential_returns_dataset = [read_row(presidential_return_headers, row) for row in presidential_returns_reader]
 
-with open(os.path.join(mit_election_lab_data_path, '1976-2018-senate.csv'), errors='replace') as senate_returns_file:
+with open('raw/1976-2018-senate.csv', errors='replace') as senate_returns_file:
     senate_returns_reader = csv.reader(senate_returns_file)
     senate_return_headers = next(senate_returns_reader)
     senate_returns_dataset = [read_row(senate_return_headers, row) for row in senate_returns_reader]
 
-with open(os.path.join(mit_election_lab_data_path, '1976-2018-house2.csv'), errors='replace') as house_returns_file:
+with open('raw/1976-2018-house2.csv', errors='replace') as house_returns_file:
     house_returns_reader = csv.reader(house_returns_file)
     house_return_headers = next(house_returns_reader)
     house_returns_dataset = [read_row(house_return_headers, row) for row in house_returns_reader]
 
-contests = {}
-contest_datasets = [presidential_returns_dataset, senate_returns_dataset, house_returns_dataset]
+contests = []
+# presidential_returns_dataset
+contest_datasets = [senate_returns_dataset, house_returns_dataset]
+
+current_contest = {'name': '', 'year': '', 'candidates': []}
+
+
+def add_contest(contest):
+    winner = contest['candidates'][0]
+    for candidate in contest['candidates'][1:]:
+        if candidate['votes'] > winner['votes']:
+            winner = candidate
+    winner['won'] = True
+
+    contests.append(contest)
+
 
 for contest_dataset in contest_datasets:
     for row in contest_dataset:
         party = row['party'].capitalize()
-        if party not in ['Democrat', 'Republican']:
-            continue
-
         year = int(row['year'])
         state = row['state']
-        office = row['office'] + (' ' + row['district'] if contest_dataset is house_returns_dataset else '')
-        if contests.get(year) is None:
-            contests[year] = {}
-        if contests[year].get(state) is None:
-            contests[year][state] = {}
-        if contests[year][state].get(office) is None:
-            contests[year][state][office] = {}
+        office = state + ' ' + row['office'] + (' ' + row['district'] if contest_dataset is house_returns_dataset else '')
 
-        contests[year][state][office]['total_votes'] = int(row['totalvotes'])
+        if office != current_contest['name']:
+            if current_contest['name'] != '':
+                add_contest(current_contest)
+            current_contest = {'name': office, 'year': year, 'candidates': []}
 
         candidate = row['candidate']
         if contest_dataset is presidential_returns_dataset:
@@ -51,7 +57,14 @@ for contest_dataset in contest_datasets:
         elif contest_dataset is house_returns_dataset or contest_dataset is senate_returns_dataset:
             candidate = candidate.replace('__', ' ')
 
-        contests[year][state][office][party] = {
-            'candidate': row['candidate'],
+        current_contest['candidates'].append({
+            'name': candidate,
+            'party': party,
+            'won': False,
             'votes': int(row['candidatevotes'])
-        }
+        })
+
+add_contest(current_contest)
+
+with open('federal_elections_all.json', 'w') as federal_elections_data_file:
+    json.dump(contests, federal_elections_data_file)
