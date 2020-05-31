e5de6f89140fb4399642c6d04f03240d5176f259
Kai Chang <kaijchang@gmail.com>
2020-05-31 16:30:32 -0700

    concept home page

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -2,9 +2,26 @@ import React from 'react';
 
 const Home: React.FunctionComponent = () => {
   return (
-    <div className='flex flex-row items-center text-white bg-gray-700'>
-        <img src='/badges/rank-BronzeTier.png' alt='Bronze' height={ 64 } width={ 64 }/>
-        <span className='rating text-2xl'>1400</span>
+    <div className='bg-gray-200 min-h-screen'>
+      <div className='text-center bg-white py-5' style={ { borderTop: 'rgb(191,13,62) solid 45px', borderBottom: 'rgb(10,49,97) solid 45px' } }>
+        <div className='flex flex-row items-center justify-center'>
+          <span className='text-2xl text-flag-blue'>★</span>
+          <h1 className='text-6xl text-bold text-yellow-400 font-big-star uppercase leading-none px-5'>
+            Firebrand
+          </h1>
+          <span className='text-2xl text-flag-blue'>★</span>
+        </div>
+        <h3 className='text-xl text-flag-red font-big-star'>ELO Ratings for U.S. Politicians</h3>
+      </div>
+      <h3 className='text-3xl text-center font-big-star py-3'>Top Politicians</h3>
+      <div className='rounded-lg bg-gray-100 w-5/6 mx-auto'>
+        <ul>
+          <li className='flex flex-row items-center text-2xl p-3'>
+            <img src='/badges/rank-GrandmasterTier.png' height={ 50 } width={ 50 }/>
+            <span className='text-outlined text-white text-3xl font-big-noodle mr-5'>3700</span> Bernie Sanders
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'big-noodle': ['Big Noodle Too', 'sans-serif'],
+        'big-star': ['Big Star', 'sans-serif']
+      },
+      textColor: {
+        'flag-red': '#bf0d3e',
+        'flag-blue': '#041e42'
+      }
+    }
   },
   variants: {},
   plugins: [],
