98134d90412123129452d96a01be9c4001d6f4b4
Kai Chang <kaijchang@gmail.com>
2020-06-03 00:53:02 -0700

    add 2020 primaries

--- trueskill/main.py
+++ trueskill/main.py
@@ -16,9 +16,37 @@ STARTING_RATING = 1500
 trueskill.setup(STARTING_RATING, STARTING_RATING / 3, STARTING_RATING / 3 / 2, STARTING_RATING / 3 / 100)
 
 _2016_PRIMARY_SCHEDULE = {
-    'Democratic': ['Iowa', 'New Hampshire', 'Nevada', 'South Carolina', 'Alabama', 'Arkansas', 'Colorado', 'Georgia', 'Massachusetts', 'Minnesota', 'Oklahoma', 'Tennessee', 'Texas', 'Vermont', 'Virginia', 'American Samoa', 'Democrats Abroad', 'Kansas', 'Louisiana', 'Nebraska', 'Maine', 'Michigan', 'Mississippi', 'CNMI', 'Florida', 'Illinois', 'Missouri', 'North Carolina', 'Ohio', 'Arizona', 'Idaho', 'Utah', 'Alaska', 'Hawaii', 'Washington', 'Wisconsin', 'Wyoming', 'New York', 'Connecticut', 'Delaware', 'Maryland', 'Pennsylvania', 'Rhode Island', 'Indiana', 'Guam', 'Nebraska', 'West Virginia', 'Kentucky', 'Oregon', 'Washington', 'U.S. Virgin Islands', 'Puerto Rico', 'California', 'Montana', 'New Jersey', 'New Mexico', 'North Dakota', 'South Dakota', 'Washington, D.C.'],
-    'Republican': ['Iowa', 'New Hampshire', 'South Carolina', 'Nevada', 'Alabama', 'Alaska', 'Arkansas', 'Colorado', 'Georgia', 'Massachusetts', 'Minnesota', 'Oklahoma', 'Tennessee', 'Texas', 'Vermont', 'Virginia', 'Wyoming', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Puerto Rico', 'Hawaii', 'Idaho', 'Michigan', 'Mississippi', 'U.S. Virgin Islands', 'Washington, D.C.', 'Wyoming', 'Guam', 'Florida', 'Illinois', 'Missouri', 'North Carolina', 'Ohio', 'CNMI', 'Arizona', 'Utah', 'American Samoa', 'Wisconsin', 'New York', 'Connecticut', 'Delaware', 'Maryland', 'Pennsylvania', 'Rhode Island', 'Indiana', 'Nebraska', 'West Virginia', 'Oregon', 'Washington', 'California', 'Montana', 'New Jersey', 'New Mexico', 'South Dakota']
+    'Democratic': ['Iowa', 'New Hampshire', 'Nevada', 'South Carolina', 'Alabama', 'Arkansas', 'Colorado', 'Georgia',
+                   'Massachusetts', 'Minnesota', 'Oklahoma', 'Tennessee', 'Texas', 'Vermont', 'Virginia',
+                   'American Samoa', 'Democrats Abroad', 'Kansas', 'Louisiana', 'Nebraska', 'Maine', 'Michigan',
+                   'Mississippi', 'CNMI', 'Florida', 'Illinois', 'Missouri', 'North Carolina', 'Ohio', 'Arizona',
+                   'Idaho', 'Utah', 'Alaska', 'Hawaii', 'Washington', 'Wisconsin', 'Wyoming', 'New York', 'Connecticut',
+                   'Delaware', 'Maryland', 'Pennsylvania', 'Rhode Island', 'Indiana', 'Guam', 'Nebraska',
+                   'West Virginia', 'Kentucky', 'Oregon', 'Washington', 'U.S. Virgin Islands', 'Puerto Rico',
+                   'California', 'Montana', 'New Jersey', 'New Mexico', 'North Dakota', 'South Dakota',
+                   'Washington, D.C.'],
+    'Republican': ['Iowa', 'New Hampshire', 'South Carolina', 'Nevada', 'Alabama', 'Alaska', 'Arkansas', 'Colorado',
+                   'Georgia', 'Massachusetts', 'Minnesota', 'Oklahoma', 'Tennessee', 'Texas', 'Vermont', 'Virginia',
+                   'Wyoming', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Puerto Rico', 'Hawaii', 'Idaho', 'Michigan',
+                   'Mississippi', 'U.S. Virgin Islands', 'Washington, D.C.', 'Wyoming', 'Guam', 'Florida', 'Illinois',
+                   'Missouri', 'North Carolina', 'Ohio', 'CNMI', 'Arizona', 'Utah', 'American Samoa', 'Wisconsin',
+                   'New York', 'Connecticut', 'Delaware', 'Maryland', 'Pennsylvania', 'Rhode Island', 'Indiana',
+                   'Nebraska', 'West Virginia', 'Oregon', 'Washington', 'California', 'Montana', 'New Jersey',
+                   'New Mexico', 'South Dakota']
 }
+_2020_PRIMARY_SCHEDULE = ['Iowa', 'New Hampshire', 'Nevada', 'South Carolina', 'Wyoming', 'Alabama', 'Arkansas',
+                          'California', 'Colorado', 'Maine', 'Massachusetts', 'Minnesota', 'North Carolina', 'Oklahoma',
+                          'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Idaho', 'Michigan', 'Mississippi',
+                          'Missouri', 'North Dakota', 'Washington', 'Arizona', 'Florida', 'Illinois', 'Arkansas',
+                          'Alaska', 'Wisconsin', 'Wyoming', 'Ohio', 'Kansas', 'Nebraska', 'Idaho', 'Oregon', 'Hawaii',
+                          'District of Columbia', 'Indiana', 'Iowa', 'Maryland', 'Montana', 'New Mexico',
+                          'Pennsylvania', 'Rhode Island', 'South Dakota', 'Georgia', 'Nevada', 'North Dakota',
+                          'South Carolina', 'West Virginia', 'Kentucky', 'Mississippi', 'New York', 'North Carolina',
+                          'South Carolina', 'Virginia', 'Colorado', 'Oklahoma', 'Utah', 'Delaware', 'New Jersey',
+                          'Louisiana', 'Alabama', 'Maine', 'Texas', 'Arizona', 'Kansas', 'Michigan', 'Missouri',
+                          'Washington', 'Tennessee', 'Hawaii', 'Connecticut', 'Georgia', 'Minnesota', 'South Dakota',
+                          'Vermont', 'Wisconsin', 'Alaska', 'Florida', 'Wyoming', 'Oklahoma', 'Massachusetts',
+                          'New Hampshire', 'Rhode Island', 'Delaware', 'Louisiana', 'Louisiana']
 
 start_year = min(*contests.distinct('year'))
 
@@ -27,17 +55,23 @@ def get_order_of_contest(contest):
     normalized_contest_name = contest['name'].lower()
 
     # Have contests that haven't occurred yet appear first
-    if contest['year'] == datetime.now().year and\
-        all(candidate['votes'] is None and not candidate['won'] for candidate in contest['candidates']):
+    if contest['year'] == datetime.now().year and \
+            all(candidate['votes'] is None and not candidate['won'] for candidate in contest['candidates']):
         return 0
     elif 'primary' in normalized_contest_name or 'caucus' in normalized_contest_name:
-        # Use the primary schedule for 2016
-        if contest['year'] == 2016 and\
-           any(candidate['name'] == 'Hillary Clinton' or candidate['name'] == 'Donald Trump' for candidate in contest['candidates']):
+        if contest['year'] == 2016 and \
+                any(candidate['name'] == 'Hillary Clinton' or candidate['name'] == 'Donald Trump' for candidate in
+                    contest['candidates']):
             contest_parts = contest['name'].split()
             party = contest_parts[-2]
             territory = ' '.join(contest_parts[:-2])
             return len(_2016_PRIMARY_SCHEDULE[party]) + 3 - _2016_PRIMARY_SCHEDULE[party].index(territory)
+        elif contest['year'] == 2020 and \
+                any(candidate['name'] == 'Joe Biden' or candidate['name'] == 'Donald Trump' for candidate in
+                    contest['candidates']):
+            contest_parts = contest['name'].split()
+            territory = ' '.join(contest_parts[:-7])
+            return len(_2020_PRIMARY_SCHEDULE) + 3 - _2020_PRIMARY_SCHEDULE.index(territory)
         return 3
     elif 'runoff' in normalized_contest_name:
         return 2
@@ -71,8 +105,6 @@ better results_input s
     - maybe cutoff candidates at some point because a lot of third party candidates are ranked 3rd and getting a lot of points b/c there are a lot of candidates in the race
 experiment with rank decay
 split like-named politicians
-make sure party is never null
-scrape more elections, especially missing presidential elections
 """
 
 
@@ -88,7 +120,7 @@ def main():
         return ratings[name]
 
     for year in range(start_year, datetime.now().year + 1):
-        contests_in_year = list(contests.find({'year': year}))
+        contests_in_year = list(contests.find({'year': year, 'hidden': None}))
         for contest in sorted(contests_in_year, key=get_order_of_contest, reverse=True):
             if len(contest['candidates']) == 0:
                 continue
@@ -111,7 +143,8 @@ def main():
 
             for (idx, candidate) in enumerate(contest['candidates']):
                 if any(term in candidate['name'].lower() for term in ['scatter', 'uncommitted']) or \
-                   candidate['name'].lower() in ['', 'n/a', 'other', 'libertarian', 'nobody', 'no', 'blank', 'null', 'void', 'miscellaneous', '--']:
+                        candidate['name'].lower() in ['', 'n/a', 'other', 'libertarian', 'nobody', 'no', 'blank',
+                                                      'null', 'void', 'miscellaneous', '--']:
                     continue
 
                 ticket = tuple(safe_get_candidate(name.strip()) for name in split_candidate_name(candidate['name']))
@@ -123,12 +156,13 @@ def main():
                 # Rank based on votes if votes data is available
                 # Otherwise, count all winners as a tie and all losers as another tie
                 results_input.append(0 if candidate['won'] else
-                                     ((vote_ranks.index(candidate['votes']) + len(contest['candidates']) - len(vote_ranks))
+                                     ((vote_ranks.index(candidate['votes']) + len(contest['candidates']) - len(
+                                         vote_ranks))
                                       if votes_recorded else 1))
 
                 tickets.append(ticket)
 
-            if len(current_ratings_input) < 2 or not any(candidate['won'] for candidate in contest['candidates']):
+            if len(current_ratings_input) < 2 or (not votes_recorded and not any(candidate['won'] for candidate in contest['candidates'])):
                 for ticket in tickets:
                     for candidate in ticket:
                         candidate['contests'].append({
@@ -151,7 +185,8 @@ def main():
                         'rating': rating_to_dict(rating)
                     })
 
-    for (idx, candidate) in enumerate(sorted(ratings.values(), key=lambda candidate: candidate['contests'][-1]['rating']['mu'], reverse=True)):
+    for (idx, candidate) in enumerate(
+            sorted(ratings.values(), key=lambda candidate: candidate['contests'][-1]['rating']['mu'], reverse=True)):
         # Pre-calculate derived fields
         candidate['ranking'] = idx + 1
         candidate['rating'] = candidate['contests'][-1]['rating']
